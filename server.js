e18295e223e1ea0a1b974142814e907ac44d89c0
Eastern Lai <easternlai@hotmail.com>
2020-03-16 16:33:07 -0700

    fixing mongo 2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect("MONGODB_URI: mongodb://heroku_q25rv3vt:8sugkd68or0kbe7buc8pslpmhr@ds051943.mlab.com:51943/heroku_q25rv3vt");
+mongoose.connect("mongodb://easternlai:Password1@ds051943.mlab.com:51943/heroku_q25rv3vt");
 
 // Start the API server
 app.listen(PORT, function() {
